f4d1d34dfc38781785b7a7df7adf913f5cbf0679
JhsK <qwe6293@nate.com>
2021-08-25 11:30:13 +0900

    색상 추가

--- front/src/theme/index.ts
+++ front/src/theme/index.ts
@@ -2,6 +2,9 @@ export const colorTheme = {
   DARK_BLUE: '#295F9E',
   MID_BLUE: '#008DD5',
   BTN_NAVY: '#082E59',
+  FONT_GRAY: '#918e8e',
+  BORDER_GRAY: '#d9d9d9',
+  HOVER_GRAY: '#fafafa',
 };
 
 export type ThemeType = typeof colorTheme;
